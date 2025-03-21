da2452ea6484b77231c60fa7834c7064d05d3505
ourtown1 <ourtown@hotmail.com>
2025-03-21 05:41:53 -0400

    Create stripred.js

---
+++ scripts/stripred.js
@@ -0,0 +1,1 @@
+javascript:(function(){x=0;s=document.styleSheets;for(const y of s){if(y.title==="applied_subreddit_stylesheet"){if(y.disabled!=true){y.disabled=true;}else{y.disabled=false;}}x++;}})();
